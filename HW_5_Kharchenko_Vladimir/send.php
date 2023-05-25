d7b0558dc4da468b15326ce295ce126ca0b1be4c
VovaMolxs <molxs@outlook.com>
2023-05-25 19:29:53 +0300

    finish

--- HW_5_Kharchenko_Vladimir/send.php
+++ HW_5_Kharchenko_Vladimir/send.php
@@ -1,16 +1,21 @@
 <?php
 function checkForm() {
     $checker = false;
-    if (!isset($_POST['send'])) return false;
-
-    if (empty($_POST['userName']) || empty($_POST['email']) || empty($_POST['numberPhone']) || empty($_POST['message'])) {
+    $message = "Ok";
+    if (!isset($_POST['send'])) {
         return false;
     }
 
-    $patternCheckNumber = '/\+[0-9]{3}\s[0-9]{2}\s[0-9]{3}-[0-9]{2}-[0-9]{2}/';
-    $patternCheckEmail = '/^[A-Za-z0-9._%+-]+@{1}[A-Za-z0-9._%+-]+.[A-Za-z]{1,5}/';
+    if (empty($_POST['userName']) || empty($_POST['email']) || empty($_POST['numberPhone']) || empty($_POST['message'])) {
+        $message = "Заполните все поля перед отправкой формы!";
+        return false;
+    }
+
+    $patternCheckNumber = '/\+[0-9]{3}\s[0-9]{2}\s[0-9]{3}-[0-9]{2}-[0-9]{2}$/';
+    $patternCheckEmail = '/^[A-Za-z0-9._%+-]+@{1}([A-Za-z0-9._%+-]{1,5}).[A-Za-z]{1,9}$/';
 
     if (preg_match($patternCheckNumber, $_POST['numberPhone'])) {
+        $message = "Неверный номер телефона!";
         $checker = true;
     } else {
         return false;
@@ -18,6 +23,7 @@ function checkForm() {
 
 
     if (preg_match($patternCheckEmail, $_POST['email'])) {
+        $message = "Неверный почтовый адресс!";
         $checker = true;
     } else {
         return false;
@@ -27,6 +33,7 @@ function checkForm() {
     $message = strip_tags($_POST['message']);
 
     if ((strlen($message) > 5 and strlen($message) <=250)) {
+        $message = "длинна сообщения должна составлять от 5 до 250 символов";
         $checker = true;
     } else {
         return false;
@@ -40,7 +47,6 @@ if (!isset($_POST['send'])) {
 }
 
 if (checkForm($_POST)) {
-   // echo var_dump(checkForm($_POST));
     header("Location: http://${_SERVER['SERVER_NAME']}/components/success.php");
 } else {
     header("Location: http://${_SERVER['SERVER_NAME']}/page.php?id=contacts");
